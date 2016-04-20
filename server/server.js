979d5e91d8481cdbe132fc214f9071c595f46de9
josh <jwentwor@gmail.com>
2016-04-20 11:49:29 -0700

    socket connecting

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
-var http = require('http');
 
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var port = process.env.PORT || 8080;
 
@@ -15,23 +15,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(bodyParser.json());
 
-var server = http.createServer(app).listen(port);
-var binaryserver = new BinaryServer({server: server, path: '/binary-endpoint'});
-
-binaryserver.on('connection', function(client) {
-  console.log('new client ---- ', client.id);
-  client.on('stream', function(stream, meta) {
-    console.log('>>>Incoming audio stream');
-
-    // broadcast to all other clients
-    for(var id in binaryserver.clients){
-      if(binaryserver.clients.hasOwnProperty(id)){
-        var otherClient = binaryserver.clients[id];
-        if(otherClient != client){
-          var send = otherClient.createStream(meta);
-          stream.pipe(send);
-        }
-      }
-    }
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
+});
+
+var server = http.listen(port, function(){
+  console.log('listening on *:3000');
 });

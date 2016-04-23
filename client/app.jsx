ebbe029bfcad1fd1716c278606e524896dceb671
josh <jwentwor@gmail.com>
2016-04-22 19:21:54 -0700

    users page

--- client/app.jsx
+++ client/app.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentTrack: {},
+      currentTrack: {url: ''},
       playlist: [],
       playIndex: null,
       loggedIn: false,

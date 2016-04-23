a605f6b3f80605dab435115ea9ebb6c8a397b6d7
controtie <dylan.ad.tran@gmail.com>
2016-04-22 18:40:15 -0700

    add landingPage

---
+++ client/landingPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from 'react-toolbox/lib/button';
+import ClassNames from 'classnames';
+import Facebook from './facebook.jsx';
+import style from './styles/toolbox-theme';
+
+class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <div className="landing-page">
+          <h1> LandingPage!</h1>
+          <Facebook login={this.props.login}/>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default LandingPage;

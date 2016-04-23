1ed06fb5de7bd69755aacc7fe68a6650e748560a
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-23 13:56:50 -0700

    Make user profile picture higher res

--- client/facebook.jsx
+++ client/facebook.jsx
@@ -55,7 +55,7 @@ class Facebook extends React.Component {
       var user = {
         name: response.name,
         fbID: response.id,
-        avatar: response.picture.data.url
+        avatar: '//graph.facebook.com/' + response.id + '/picture?type=large'
       }
       this.props.login(user);
     }.bind(this));

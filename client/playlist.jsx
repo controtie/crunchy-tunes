6b0be84010b7dd818497353f736b122adf03beed
Wilson Yu <wilsonweichengyu@gmail.com>
2016-04-23 14:07:11 -0700

    fixed css

--- client/playlist.jsx
+++ client/playlist.jsx
@@ -5,23 +5,33 @@ import ClassNames from 'classnames';
 import style from './styles/toolbox-theme';
 
 const Playlist = ({ playlist, currentSongIndex, handleClick, remove }) => {
-  let cards = playlist.map((track, index) =>
+  var cards = playlist.map( function (track, index) {
+
+    var updateTitle = track.songTitle;
+    if(track.songTitle.length > 40) {
+      updateTitle = track.songTitle.substring(0,39) + "...";
+    }
+
+    return (
     <Card
       className={ClassNames(style['card'])}
       className={currentSongIndex === index ? ClassNames(style['current-card']) : null}
       key={track.contentId}
-      style={{ width: '350px', height: '80px', margin: '4px' }}
+      style={{ width: '350px', height: '100px', margin: '5px' }}
     >
       <CardTitle
-            className={ClassNames(style['playlistCard'])}
-            subtitle={track.songTitle}
+            className={ClassNames(style['source-logo-for-playlist'])}
+            avatar="https://c1.staticflickr.com/9/8082/8292777643_65090144e9.jpg"
+            subtitle={updateTitle}
       />
       <CardActions>
         <Button label="Remove" className={ClassNames(style['remove'])} onClick={() => remove(index)} />
         <Button label="Play"  className={ClassNames(style['play'])} onClick={() => handleClick(track, index, true)} />
       </CardActions>
     </Card>
-  );
+    )
+  });
+
   return (
     <div className="playlist">
       {cards}
@@ -30,3 +40,8 @@ const Playlist = ({ playlist, currentSongIndex, handleClick, remove }) => {
 };
 
 export default Playlist;
+
+
+
+
+
